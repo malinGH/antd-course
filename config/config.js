31f1cdea375bf3782f051f7bb10523c69b063a34
malin@souche.com <malin@souche.com>
2019-04-11 15:07:49 +0800

    test-antd

--- config/config.js
+++ config/config.js
@@ -5,14 +5,28 @@ export default {
             antd: true
         }],
     ],
-    routes: [{
-        path: '/',
-        component: '../layout',
-        routes: [
-            {
-                path: 'helloworld',
-                component: './HelloWorld'
-            }
-        ]
-    }]
+    routes: [
+        {
+            path: '/',
+            component: '../layout/index',
+            routes: [
+                {
+                    path: '/',
+                    component: 'HelloWorld'
+                },
+                {
+                    path: 'helloworld',
+                    component: 'HelloWorld'
+                },
+                {
+                    path: '/dashboard',
+                    routes: [
+                      { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+                      { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+                      { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+                    ]
+                  },
+            ]
+        },
+    ],
 }

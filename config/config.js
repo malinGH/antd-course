97cec564b6fec68a3408c878156bd9b9027c4a74
malin@souche.com <malin@souche.com>
2019-04-12 19:07:34 +0800

    测试model调用

--- config/config.js
+++ config/config.js
@@ -2,7 +2,8 @@ export default {
     plugins: [
         ['umi-plugin-react', {
             // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
-            antd: true
+            antd: true,
+            dva: true,
         }],
     ],
     routes: [
@@ -21,11 +22,15 @@ export default {
                 {
                     path: '/dashboard',
                     routes: [
-                      { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
-                      { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
-                      { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+                        { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+                        { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+                        { path: '/dashboard/workplace', component: 'Dashboard/Workplace' },
                     ]
-                  },
+                },
+                {
+                    path: 'puzzlecards',
+                    component: 'Puzzlecards'
+                },
             ]
         },
     ],
